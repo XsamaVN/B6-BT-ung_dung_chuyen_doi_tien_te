10e70c3a1f7c5ec4baed4f7d0064a74840659aa7
XsamaVN <buianhduc97@gmail.com>
2023-10-09 11:33:16 +0700

    app chuyen doi tien te v2

--- codeJS.js
+++ codeJS.js
@@ -1,37 +1,7 @@
 function chuyendoi() {
-    let Amount = document.getElementById("Amount").value;
-    let FromC = document.getElementById("From").value;
-    let To = document.getElementById("To").value;
-    let Result;
-
-
-    if (FromC == "USD" && To == "VND") {
-        Result = "Result: " + (Amount * 23000) + " Đ";
-    }
-    else if (FromC == "VND"&& To == "USD"){
-        Result = "Result: "+ (Amount / 23000) + " $";
-    }
-    else if (FromC == "VND"&& To == "Yen"){
-        Result = "Result: "+ (Amount / 164.260) + " ¥";
-    }
-    else if (FromC == "Yen"&& To == "VND"){
-        Result = "Result: "+ (Amount * 164.260) + " Đ";
-    }
-    else if (FromC == "Yen"&& To == "USD"){
-        Result = "Result: "+ (Amount * 148.91) + " $";
-    }
-    else if (FromC == "USD"&& To == "Yen"){
-        Result = "Result: "+ (Amount * 148.91) + " ¥";
-    }
-    else if (FromC == "VND" && To == "VND") {
-        Result = "Result: " + Amount + " Đ"
-    }
-    else if (FromC == "Yen" && To == "Yen") {
-        Result = "Result: " + Amount + " ¥"
-    }
-    else {
-        Result = "Result: " + Amount + " $"
-    }
-
-    document.getElementById("Result").innerHTML = Result;
+   let a = document.getElementById('From').value;
+   let b = document.getElementById('To').value;
+   let amount = document.getElementById('Amount').value;
+   let kq = amount * a / b;
+   document.getElementById('Result').innerHTML = kq;
 }
